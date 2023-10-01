6c4d21f3a07f4ea87f1c23c146c2c6aafdc0ceba
jp <145996135+jpamong@users.noreply.github.com>
2023-10-01 04:35:20 -0700

    Add files via upload

--- Philippineslandingpage/main.js
+++ Philippineslandingpage/main.js
@@ -26,11 +26,11 @@ sideNav.style.right = "-250px";
 menuBtn.onclick = function(){
   if(sideNav.style.right == "-250px"){
      sideNav.style.right = "0";
-     menu.src = "../images/close.png";
+     menu.src = "/Images/close.png";
   }
   else{
     sideNav.style.right = "-250px";
-    menu.src = "../images/menu.png";
+    menu.src = "/Images/menu.png";
   }
 }
 // END SIDENAVBAR JAVASCIRPT
