32e0f4712f727efdd16081b04aa91dad84943783
jp <145996135+jpamong@users.noreply.github.com>
2023-11-20 08:28:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,8 @@ const loader = document.getElementById("preloader");
 
 window.addEventListener("load", function(){
   loader.style.display = "none";
+  document.body.style.overflow = "unset";
+  
 
 });
 
